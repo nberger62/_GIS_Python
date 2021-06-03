671130eb66310b61012816bfc6140c554d3bd138
Nathan Berger <63067997+nberger62@users.noreply.github.com>
2021-06-03 18:19:49 -0400

    Fixed the Mod3 LAb python script by deleting for loop, fixing the getmessage to get messages for the terminal output, and I corrected the arcpy.Buffer analysis tool with fixing the 1,000 meters instead of feet. I deleted some print statements that were not necessary, and then added the /n in my print statements for concatenating the strings to make a space between each tool that is running.

--- Module_3/Scripts/Mod3_NAB.py
+++ Module_3/Scripts/Mod3_NAB.py
@@ -1,44 +1,36 @@
 # Name: Nathan Berger
 # Date: 6/1/2021
-# Class:
-# Script Name:
+# Class: GIS Programming
+# Script Name: Mod3_NAB.py
+# Description:
+# This Python script takes a shapefile of hospitals, adds XY coordinates or LAT/Long's to the points,
+# then establishes a buffer of 1000 meters around the hospitals,
+# and the dissolves the buffer to make sure lines do not overlap the buffered area around the hospitals.
+# Each step is logged in the terminal with the script's start time, end time, and the tool's name that is running at that point in time.
 #----------------------------------------------------------
+
 # import modules
 import arcpy
 
 # Set up Workspace Environment
-
-arcpy.overwriteOutput = True
+arcpy.env.overwriteOutput = True
 arcpy.env.workspace = r"C:\Users\Admin\PycharmProjects\UWF\Module_3\Data\Mod3_Data"
 
-# Step 1: I need to run the tool for Adding X,Y coordinates to my shapefile called hospitals
-# This step needs to have the input shapefile, and set the output shapefile by copyign the data and then adding the points.
+# I need to run the tool for Adding X,Y (Longitude and Lattitude) coordinates to my shapefile called hospitals.
+# I will copy the shapefile first and then add the coordinates.
+# This step needs to have the input shapefile, and set the output shapefile by copying the data and then adding the points.
 arcpy.Copy_management("hospitals.shp", "hospitalsXYpts.shp")
 print("Adding XY Coordinates to the shapefile...")
-for i in range(0, arcpy.GetMessageCount()):
-    arcpy.AddReturnMessage(i)
-
 arcpy.AddXY_management("hospitalsXYpts.shp")
-print("hospitalsXYpts.shp \n")
+print(arcpy.GetMessages() + "\n")
 
+# Now we are going to run an arcpy analysis tool to buffer 1,000 meters around the hosptials we identified earlier in the script.
 print("Executing buffer analysis on the shapefile...")
-for i in range(0, arcpy.GetMessageCount()):
-    arcpy.AddReturnMessage(i)
+arcpy.analysis.Buffer("hospitalsXYpts.shp", "hosXY_buffer", "1000 Meters")
+print(arcpy.GetMessages() + "\n")
 
-# Now we are going to run an arcpy analysis tool to buffer 1,000 meters around the hosptials we identified earlier in the script
-
-arcpy.analysis.Buffer("hospitalsXYpts.shp", "hosXY_buffer", 1000)
-print("hosXY_buffer \n")
-
+# Now we can dissolve the buffer around the hospitals into it's own feature class file saved into our project's GDB.
+# The dissolve tool will elimanate any overlapping lines that the buffer ranges have created around the hospitals.
 print("Executing the dissolve analysis on the shapefile...")
-for i in range(0, arcpy.GetMessageCount()):
-    arcpy.AddReturnMessage(i)
-
-# Now we can dissolve the buffer around the hospitals into it's own feature class file saved into our project's GDB
-
-with arcpy.EnvManager(scratchWorkspace=r"C:\Users\Admin\PycharmProjects\UWF\Module_3\Data\Mod3_Data\Mod3_NAB\Mod3_NAB.gdb", workspace=r"C:\Users\Admin\PycharmProjects\UWF\Module_3\Data\Mod3_Data\Mod3_NAB\Mod3_NAB.gdb"):
-    arcpy.management.Dissolve(r"C:\Users\Admin\PycharmProjects\UWF\Module_3\Data\Mod3_Data\hosXY_buffer.shp", r"C:\Users\Admin\PycharmProjects\UWF\Module_3\Data\Mod3_Data\Mod3_NAB\Mod3_NAB.gdb\hosXY_buffer_Dissolve", None, None, "MULTI_PART", "DISSOLVE_LINES")
-print("hosXY_buffer_dissolve \n")
-
-for i in range(0, arcpy.GetMessageCount()):
-    arcpy.AddReturnMessage(i)
+arcpy.management.Dissolve("hosXY_buffer.shp", "hosXY_buffer_Dissolve", None, None, "MULTI_PART", "DISSOLVE_LINES")
+print(arcpy.GetMessages() + "\n")
